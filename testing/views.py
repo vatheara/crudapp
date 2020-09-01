2caa0e2bf9b7d7516abad37e053f1febfe845229
Thearith Psy <themacstore@Thes-MacBook-Pro.local>
2020-09-01 16:32:47 +0700

    can not return index.html in testing app

--- testing/views.py
+++ testing/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return HttpResponse("hello world")
+def hello(request):
+    return render(request,'index.html')
