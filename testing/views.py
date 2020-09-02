88ddada4ffdac649f98aa9ee83789bf38051fe90
Noobie <theara2048@gmail.com>
2020-09-02 22:50:01 +0700

    there'er two apps using one templates directory

--- testing/views.py
+++ testing/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("hello world")
 def hello(request):
-    return render(request,'index.html')
+    return render(request,'hello.html')
