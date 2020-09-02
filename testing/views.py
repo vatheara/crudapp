a320fa43c4e33147305ab37892289d1313bac2f4
Noobie <theara2048@gmail.com>
2020-09-02 22:55:27 +0700

    fixed! just add app in setting.py

--- testing/views.py
+++ testing/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("hello world")
 def hello(request):
-    return render(request,'hello.html')
+    return render(request,'helloworld.html')
